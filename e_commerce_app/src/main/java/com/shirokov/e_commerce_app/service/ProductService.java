3f84c17409ce58eef0c09c004f8468efbdaf6acf
OldinPadawan <diyandmakers@gmail.com>
2024-01-08 20:43:34 +0300

    implements Product controller

--- e_commerce_app/src/main/java/com/shirokov/e_commerce_app/service/ProductService.java
+++ e_commerce_app/src/main/java/com/shirokov/e_commerce_app/service/ProductService.java
@@ -21,7 +21,7 @@ public interface ProductService {
 
     public List<Product> findProductByCategory(String category);
 
-    public Page<Product> getAllProduct(String category, List<String> colors, List<Size>sizes,
+    public Page<Product> getAllProduct(String category, List<String> colors, List<String>sizes,
                                        Integer minPrice, Integer maxPrice, Integer minDiscount, String sort, String stock,
                                        Integer pageNumber, Integer pageSize);
 
